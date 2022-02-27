60f9d81f99666c7eaba46c9fd947610cc5c68399
Michael N. Gagnon <mng@Michaels-MacBook-Pro.local>
2022-02-27 08:02:02 -0500

    .

--- js/slots.js
+++ js/slots.js
@@ -1,3 +1,7 @@
+const IMG_MANIFEST = [
+    {src: "img/7.png", id: "7"},
+    {src: "img/Cherry.png", id: "Cherry"},
+]
 
 class SlotMachine {
 
@@ -5,8 +9,9 @@ class SlotMachine {
         this.slotsContainerId = slotsContainerId;
         this.slotsCanvasId = slotsCanvasId;
         this.tryAgainContainerId = tryAgainContainerId;
+        this.images = {};
         this.initStage();
-        this.loadBitmaps();
+        //this.loadBitmaps();
     }
 
     initStage() {
@@ -14,32 +19,65 @@ class SlotMachine {
         this.stage = new createjs.StageGL(this.slotsCanvasId);
         this.stage.setClearColor("#000");
         createjs.Ticker.addEventListener("tick", this.stage);
+    }
 
-        // var circle = new createjs.Shape();
-        // circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-        // circle.x = 0;
-        // circle.y = 0;
-        // this.stage.addChild(circle);
-        // this.stage.update();
+    loadBitmap(name) {
+        const bmp = new createjs.Bitmap(IMAGES[name]);
+        const THIS = this;
+        this.bmp.image.onload = function() {
+
+            // console.log(2)
+            // THIS.stage.addChild(THIS.bmp)
+            // THIS.bmp.x = 0;
+            // THIS.bmp.y = 0;
+            // THIS.stage.update();
+
+        }        
     }
 
     loadBitmaps() {
+
+        return;
         this.bmp = new createjs.Bitmap("img/7.png");
         const THIS = this;
         this.bmp.image.onload = function() {
-            console.log(2)
-            THIS.stage.addChild(THIS.bmp)
-            THIS.bmp.x = 0;
-            THIS.bmp.y = 0;
-            THIS.stage.update();
+
+            // console.log(2)
+            // THIS.stage.addChild(THIS.bmp)
+            // THIS.bmp.x = 0;
+            // THIS.bmp.y = 0;
+            // THIS.stage.update();
 
         }
+
+    }
+
+    run() {
 
     }
 }
 
 function main() {
     const machine = new SlotMachine("slots-container", "slots-canvas", "try-again-container");
+
+    const queue = new createjs.LoadQueue();
+    queue.on("complete", function(event) {
+        //machine.images["7"] = new createjs.Bitmap(queue.getResult("7"));
+        for (record of IMG_MANIFEST) {
+            //src = record.src;
+            id = record.id;
+            machine.images[id] = new createjs.Bitmap(queue.getResult(id));
+            machine.stage.addChild(machine.images[id]);
+        }
+        machine.run();
+        // Do stuff with bitmap
+    });
+
+
+
+    queue.loadManifest(IMG_MANIFEST);
+
+
 }
 
 main();
