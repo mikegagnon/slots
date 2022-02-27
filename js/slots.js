3561a281687af3e64fd00228872a809e72856df3
Michael N. Gagnon <mng@Michaels-MacBook-Pro.local>
2022-02-27 07:23:20 -0500

    .

--- js/slots.js
+++ js/slots.js
@@ -1,19 +1,32 @@
 
 class SlotMachine {
 
-    constructor(slotsContainerId, tryAgainContainerId) {
+    constructor(slotsContainerId, slotsCanvasId, tryAgainContainerId) {
         this.slotsContainerId = slotsContainerId;
+        this.slotsCanvasId = slotsCanvasId;
         this.tryAgainContainerId = tryAgainContainerId;
         this.init();
     }
 
     init() {
         console.log(1);
+        this.stage = new createjs.Stage(this.slotsCanvasId);
+
+
+
+        var circle = new createjs.Shape();
+        circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+        circle.x = 0;
+        circle.y = 0;
+        this.stage.addChild(circle);
+        this.stage.update();
+
+
     }
 }
 
 function main() {
-    const machine = new SlotMachine("#slots-container", "try-again-container");
+    const machine = new SlotMachine("slots-container", "slots-canvas", "try-again-container");
 }
 
 main();
