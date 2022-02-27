2b4063225dcab5d382f5c323cb960f538e4ef095
Michael N. Gagnon <mng@Michaels-MacBook-Pro.local>
2022-02-27 07:46:29 -0500

    .

--- js/slots.js
+++ js/slots.js
@@ -5,22 +5,35 @@ class SlotMachine {
         this.slotsContainerId = slotsContainerId;
         this.slotsCanvasId = slotsCanvasId;
         this.tryAgainContainerId = tryAgainContainerId;
-        this.init();
+        this.initStage();
+        this.loadBitmaps();
     }
 
-    init() {
+    initStage() {
         console.log(1);
-        this.stage = new createjs.Stage(this.slotsCanvasId);
+        this.stage = new createjs.StageGL(this.slotsCanvasId);
+        this.stage.setClearColor("#000");
+        createjs.Ticker.addEventListener("tick", this.stage);
 
+        // var circle = new createjs.Shape();
+        // circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+        // circle.x = 0;
+        // circle.y = 0;
+        // this.stage.addChild(circle);
+        // this.stage.update();
+    }
 
+    loadBitmaps() {
+        this.bmp = new createjs.Bitmap("img/7.png");
+        const THIS = this;
+        this.bmp.image.onload = function() {
+            console.log(2)
+            THIS.stage.addChild(THIS.bmp)
+            THIS.bmp.x = 0;
+            THIS.bmp.y = 0;
+            THIS.stage.update();
 
-        var circle = new createjs.Shape();
-        circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-        circle.x = 0;
-        circle.y = 0;
-        this.stage.addChild(circle);
-        this.stage.update();
-
+        }
 
     }
 }
